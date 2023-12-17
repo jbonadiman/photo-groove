1b7dad964b279113a5c68ae64b6234cb4080c257
João Vitor Bonadiman <18357636+jbonadiman@users.noreply.github.com>
2023-12-17 18:49:10 +0000

    adds type annotations

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,13 +1,28 @@
 module PhotoGroove exposing (main)
 
 import Browser
-import Html exposing (div, h1, img, text)
+import Html exposing (div, h1, img, text, Html)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Array exposing (Array)
 
+
+type alias Photo =
+  { url : String }
+
+type alias Model =
+  { photos : List Photo
+  , selectedUrl : String
+  }
+
+type alias Message =
+  { description : String, data : String }
+
+
+urlPrefix : String
 urlPrefix = "https://elm-in-action.com/"
 
-
+view : Model -> Html Message
 view model = 
   div [ class "content" ]
     [ h1 [] [text "Photo Groove" ]
@@ -20,14 +35,22 @@ view model =
         []
     ]
 
+
+selectPhoto : Message
+selectPhoto = { description = "ClickedPhoto", data = "1.jpeg" }
+
+
+viewThumbnail : String -> Photo -> Html Message
 viewThumbnail selectedUrl thumb =
   img
     [ src (urlPrefix ++ thumb.url)
     , classList [ ( "selected", selectedUrl == thumb.url ) ]
-    , onClick { description = "ClickedPhoto", data = thumb.url }
+    , onClick { selectPhoto | data = thumb.url }
     ]
     []
 
+
+initialModel : Model
 initialModel =
   { photos =
     [ { url = "1.jpeg" }
@@ -38,12 +61,19 @@ initialModel =
     , selectedUrl = "1.jpeg"
   }
 
+
+photoArray : Array Photo
+photoArray = Array.fromList initialModel.photos
+
+
+update : Message -> Model -> Model
 update msg model =
   if msg.description == "ClickedPhoto" then
     { model | selectedUrl = msg.data }
 
   else
     model
+
 
 main =
   Browser.sandbox
